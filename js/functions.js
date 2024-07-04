0875ebf0b1662dc1ef5db41e1b55638e2f8fd4b2
whitekz88 <job.beloshapkin@gmail.com>
2024-07-04 17:38:21 +0500

    Реализует функции проверки длины строк, полиндром и извлечение числа

---
+++ js/functions.js
@@ -0,0 +1,28 @@
+// Функция проверки длины строки
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+
+// Функция по опредлению, является ли строка полиндромом
+const polindrome = (string) => {
+  string = string.toLowerCase().replaceAll(' ', '');
+  let oppositeString = '';
+
+  for(let character = string.length - 1;character >= 0;character--) {
+    oppositeString += string[character];
+  }
+  return string === oppositeString;
+};
+
+// Дополнительное задание. Функция по извелечению целого числа
+const extractNumber = (string) => {
+  let resaultNumber = '';
+  if(typeof(string) === 'number') {
+    string = string.toString();
+  }
+  for(let number = 0; number <= string.length - 1; number++){
+    if(!Number.isNaN(parseInt(string[number], 10))){
+      resaultNumber += string[number] ;
+    }
+  }
+  return resaultNumber = parseInt(resaultNumber, 10);
+};
+
